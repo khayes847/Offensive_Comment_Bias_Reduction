8512731c2d19624bea78373361f7a6dd02e6fb3d
Kyle Hayes <khayes847@gmail.com>
2019-11-08 13:11:23 -0500

    Finalize project files

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Offensive_Comment_Bias_Reduction
+
+## Created By:
+
+* __Kyle Hayes__
+
+## Project Details:
+
+Existing offensive speech detection models often learn to categorize discussion of marginalized identity groups with attacks against these groups themselves. As a result, these models retain significant biases against minority racial, ethnic, gender, and sexual identities. The purpose of this project is to create an offensive speech detection model for use on comment boards that minimizes false 'offensive' classifications by predicting not only whether a comment is offensive, but whether it relates to a specific identity group as well.
+
+## Process Breakdown:
+
+- **Business Understanding**:
+  Internet comment boards have become an extremely popular way not only for people to communicate via social network, but for businesses to build relationships with their consumers. These businesses have incentive to find an automated model that remove offensive comments as quickly as possible. However, given the potential negative publicity that can surround machine learning systems that have accidentally internalized prejudice against identity groups, these businesses arguably have a greater incentive to insure that their machine learning systems minimize incorrect 'offensive' assignations.
+
+- **Data Understanding**:
+  I obtained my data from Kaggle's "Jigsaw Unintended Bias in Toxicity Classification" training dataset (https://www.kaggle.com/c/jigsaw-unintended-bias-in-toxicity-classification/data). It consists of 1.8 million Quora comments, which Google annotators have labelled according to their offensiveness. The resulting 'target' is the percentage of commentators who believed the comment to be offensive. In addition, roughly 405 thousand comments contained annotations regarding whether the comment specifically related to an identity group. For example, if a comment had the value '0.4' for 'female', it meant that 40% of annotators believed that the comment related to women as an identity. For my dataset, I used this subsection.
+  In addition to these values, the data contains information about the comment sections, labels regarding specific types of offensiveness ('obscene', 'insult', etc.), as well as offensiveness labels obtained from a prior platform.
+
+- **Data Preparation**:
+  I will first remove the 1.4 million comments without identity annotations. I will binarize the 'target' variable by classifying any value equal to or greater than 0.5 as offensive, and I will binarize each 'identity' variable by classifying any value equal to or greater than 0.15 as relating to the identity. I will then create a second multinary 'target' variable that describes both whether the comment is offensive and whether it relates to at least one 'identity'. After separating the two target variables from the other features, I will then remove all features except for the comments themselves, and clean the text in preparation for vectorization.
+
+- **Modeling**:
+  To determine our baseline, I will vectorize the words in each comment, and perform a simple logistic regression using only the binary 'offensive' categorization as a target. For our final model, I will use a multinary target that reflects both offensiveness and the presence of 'identity group' content. In addition, this model will use a Keras deep learning algorithm.
+
+- **Evaluation**:
+  Since the inoffensive/offensive comment ratio is imbalanced, and since we have an interest in minimizing false 'offensive' assignations more than false 'inoffensive' assignations, I will use a custom cost function that penalizes/rewards each categorization appropriately. However, when searching for parameters, I will use the F1-score for the sake of simplicity to find the parameters that best balance our precision and recall. 
+
+- **Deployment**:
+  I will save and make available my final Keras model.
+
+## Files in Repository:
+
+* __README.md__ - A summary of the project.
+
+* __technical_notebook.ipynb__ - Step-by-step walk through of the modeling/optimization process with rationale explaining decisions made. After cleaning and preparing the data, the notebook uses a gridsearch to create a logistic regression of the vectorized comments, and determines a baseline score using a custom cost function. For comparison, it also shows the score using only test data that contains 'identity' content. It then shows the process of creating a Keras deep-learning model, and compares the resulting scores (both for the entire test dataset and for only 'identity group' comments) to the prior model.
+
+* __data_prep.py__ - Gathers and prepares data for analysis.
+
+* __functions.py__ - Provides general functions used in the technical notebook, including Keras modeling and cost function calculation.
+
+* __train_bias.csv__ - The raw dataset prior to data preparation.
+
+* __target.csv__ - The cleaned target variables.
+
+* __target.csv__ - The cleaned feature variables.
+
+* __presentation.pptx__ - a presentation detailing the project and its results.
--- functions.py
+++ functions.py
@@ -6,28 +6,28 @@ Created on Thurs Oct 10 10:28 2019
 @author: khayes847
 """
 
+import itertools
 import pandas as pd
 import numpy as np
 # pylint: disable=unused-import
 import swifter
 # pylint: enable=unused-import
 import matplotlib.pyplot as plt
-import itertools
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
-from sklearn.dummy import DummyClassifier
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from sklearn.metrics import recall_score, precision_score
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_extraction.text import CountVectorizer
-from yellowbrick.classifier import ConfusionMatrix
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import nltk
 from nltk.corpus import stopwords
-#nltk.download('wordnet')
-#nltk.download('punkt')
-#nltk.download('stopwords')
+# nltk.download('stopwords')
 from keras import backend as K
+# pylint: disable=unused-import
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.text import Tokenizer
+from keras import models, layers, optimizers
+from keras.layers import Dense, Dropout, Activation
+# pylint: enable=unused-import
 
 
 def reduce_mem_usage(data):
@@ -87,13 +87,14 @@ def xyvalues():
     return x_val, y_val
 
 
-def test_group(model, y_test, x_val, y_val, vectorize==True):
-    """Returns test section with group classification"""
+def test_data_group(model, y_test, x_val, y_val, vectorize=True):
+    """Returns test section with group classification for
+    initial logistic regression"""
     x_val = x_val[x_val.index.isin(y_test.index)]
     y_val = y_val[y_val.index.isin(y_test.index)]
     data = pd.concat([y_val, x_val], axis=1)
-    data = data.loc[(data.offensive_and_identity==1)|
-                    (data.offensive_and_identity==3)]
+    data = data.loc[(data.offensive_and_identity == 1) |
+                    (data.offensive_and_identity == 3)]
     x_group = data[['comment_text']]
     y_group = data['offensive']
     clean_group_data = []
@@ -101,14 +102,25 @@ def test_group(model, y_test, x_val, y_val, vectorize==True):
         clean_group_data.append(testdata)
     if vectorize:
         clean_group_data = model.transform(clean_group_data)
-    return clean_group_data, y_group 
+    return clean_group_data, y_group
 
 
-def train_test(x_val, y_val, test=.33, rs=42):
+def keras_group(x_test, y_test):
+    """Returns identity section of test labelled as group comments
+    for keras analysis"""
+    data = pd.concat([y_test, x_test], axis=1)
+    data = (data.loc[(data.offensive_and_identity == 1) |
+                     (data.offensive_and_identity == 3)])
+    x_group = data[['comment_text']]
+    y_group = data['offensive_and_identity']
+    return x_group, y_group
+
+
+def train_test(x_val, y_val, test=.33, rs_val=42):
     """Seperates values into train and test data"""
     x_train, x_test, y_train, y_test = train_test_split(x_val, y_val,
                                                         test_size=test,
-                                                        random_state=rs,
+                                                        random_state=rs_val,
                                                         stratify=y_val)
     return x_train, x_test, y_train, y_test
 
@@ -120,61 +132,19 @@ def scores(y_test, y_pred):
     print('Test Cost Function Score: ', cost_function(y_test, y_pred))
 
 
-def vader(x_val, s_type):
-    """Returns specified vader score"""
-    analyser = SentimentIntensityAnalyzer()
-    score = analyser.polarity_scores(x_val)
-    return score[s_type]
-
-
-def sentiment_initial(data):
-    """Assigns vader score columns"""
-    data['compound_initial'] = (data.comment_text
-                                .swifter.apply
-                                (lambda x:
-                                 vader(x, 'compound')))
-    return data
-
-
-def recall_k(y_true, y_pred):
-    """Creates custom recall measurement for Keras"""
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
-
-def precision_k(y_true, y_pred):
-    """Creates custom precision measurement for Keras"""
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
-    
-def f1_k(y_true, y_pred):
+def f1_score_custom(y_true, y_pred):
     """Creates custom F1 measurement for Keras"""
-    precision = precision_m(y_true, y_pred)
-    recall = recall_m(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
     f1_val = 2*((precision*recall)/(precision+recall+K.epsilon()))
     return f1_val
 
 
-def cost_function_k(y_true, y_pred):
-    """Creates custom cost function measurement for Keras"""
-    true_pos = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    pred_pos = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    total_pos = K.sum(K.round(K.clip(y_true, 0, 1)))
-    false_pos = pred_pos-true_pos
-    false_neg = total_pos-true_pos
-    true_neg = int((len(y_true))-(true_pos+false_pos+false_neg))
-    score = 0
-    score = (4*false_pos)+false_neg-(2*true_pos)-(0.0005*true_neg)
-    return score
-    
-
-def cost_function(y_true, y_pred):
+def cost_function(y_true, y_pred, multinary=False):
     """Creates score for custom cost function"""
+    if multinary:
+        y_true = change(y_true)
+        y_pred = change(y_pred)
     pred_labels = np.asarray(y_pred)
     true_labels = np.asarray(y_true)
     true_pos = np.sum(np.logical_and(pred_labels == 1, true_labels == 1))
@@ -186,24 +156,26 @@ def cost_function(y_true, y_pred):
     return score
 
 
-def plot_confusion_matrix(cm, classes, normalize=False, title="Confusion Matrix"):
+def plot_confusion_matrix(cm_val, classes, normalize=False,
+                          title="Confusion Matrix"):
     """Creates graph of confusion matrix"""
     plt.grid(None)
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.imshow(cm_val, interpolation='nearest', cmap=plt.cm.Blues)
     plt.title(title)
     plt.colorbar()
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation='45')
     plt.yticks(tick_marks, classes)
     if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        cm_val = cm_val.astype('float') / cm_val.sum(axis=1)[:, np.newaxis]
         print("Normalized confusion matrix")
     else:
         print('Confusion matrix, without normalization')
-    thresh = cm.max()/2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j,i, cm[i,j], horizontalalignment="center",
-                 color="white" if cm[i,j]>thresh else "black")
+    thresh = cm_val.max()/2
+    for i, j in itertools.product(range(cm_val.shape[0]),
+                                  range(cm_val.shape[1])):
+        plt.text(j, i, cm_val[i, j], horizontalalignment="center",
+                 color="white" if cm_val[i, j] > thresh else "black")
     plt.tight_layout()
     plt.ylabel('True \nlabel', rotation=0)
     plt.xlabel('Predicted label')
@@ -211,8 +183,7 @@ def plot_confusion_matrix(cm, classes, normalize=False, title="Confusion Matrix"
 
 
 def visualize_training_results(results):
-    """Creates graph that visualizes loss and F1 score measurements at each
-    epoch during Keras modeling"""
+    """Creates graph that visualizes Keras measurements at each epoch"""
     history = results.history
     plt.figure()
     plt.plot(history['val_loss'])
@@ -222,11 +193,11 @@ def visualize_training_results(results):
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.show()
-    
+
     plt.figure()
-    plt.plot(history['val_f1_m'])
-    plt.plot(history['f1_m'])
-    plt.legend(['val_f1_m', 'f1_m'])
+    plt.plot(history['val_f1_score'])
+    plt.plot(history['f1_score'])
+    plt.legend(['val_f1_score', 'f1_score'])
     plt.title('F1 Scores')
     plt.xlabel('Epochs')
     plt.ylabel('F1 Scores')
@@ -236,24 +207,19 @@ def visualize_training_results(results):
 def vectorize_initial(x_train, x_test):
     """Creates count vectorizer model, fits it to the X_train dataset, and
     transforms the X train and test datasets."""
-    
     stopwords_set = set(stopwords.words('english'))
-    
     clean_train_data = []
     for traindata in x_train['comment_text']:
         clean_train_data.append(traindata)
-
     clean_test_data = []
     for testdata in x_test['comment_text']:
         clean_test_data.append(testdata)
-    
     vectorizer = CountVectorizer(analyzer="word",
                                  tokenizer=None,
                                  preprocessor=None,
                                  stop_words=stopwords_set,
                                  max_features=6000,
                                  ngram_range=(1, 3))
-
     train_features = vectorizer.fit_transform(clean_train_data)
     test_features = vectorizer.transform(clean_test_data)
     return vectorizer, train_features, test_features
@@ -261,9 +227,10 @@ def vectorize_initial(x_train, x_test):
 
 def log_gridsearch(train_features, y_train):
     """Returns gridsearch result for Logistic Regression"""
-    textreg = LogisticRegression(fit_intercept = False, solver='saga', random_state=42)
-    C = [1,3,5,7,9]
-    hyperparameters = dict(C=C)
+    textreg = LogisticRegression(fit_intercept=False, solver='saga',
+                                 random_state=42)
+    c_val = [1, 3, 5, 7, 9]
+    hyperparameters = dict(C=c_val)
     clf = GridSearchCV(textreg, hyperparameters, cv=3, n_jobs=-2)
     best_model = clf.fit(train_features, y_train)
     print('Best C:', best_model.best_estimator_.get_params()['C'])
@@ -271,7 +238,8 @@ def log_gridsearch(train_features, y_train):
 
 def logreg(train_features, y_train, c_val):
     """Returns fitted logistic regression model"""
-    textreg = LogisticRegression(C=c_val, fit_intercept = False, solver='saga', random_state=42)
+    textreg = LogisticRegression(C=c_val, fit_intercept=False,
+                                 solver='saga', random_state=42)
     textreg.fit(train_features, y_train)
     return textreg
 
@@ -282,4 +250,110 @@ def logreg_cm(model, test_features, y_test, cm_labels):
     cm_val = confusion_matrix(y_test, y_pred)
     plot_confusion_matrix(cm_val, cm_labels)
     scores(y_test, y_pred)
-    
+
+
+def tokenizer_onehot(x_val, y_val):
+    """Returns feature and target models for tokenizing
+    and converting comments to matrix"""
+    data = pd.concat([y_val, x_val], axis=1)
+    comments = data["comment_text"]
+    tokenizer = Tokenizer(num_words=6000)
+    tokenizer.fit_on_texts(comments)
+    one_hot_results = tokenizer.texts_to_matrix(comments, mode='binary')
+
+    target = data['offensive_and_identity']
+    le_val = preprocessing.LabelEncoder()
+    le_val.fit(target)
+    target_cat = le_val.transform(target)
+    target_onehot = to_categorical(target_cat)
+    return one_hot_results, target_onehot
+
+
+def keras_prep(one_hot_results, target_onehot, x_test, group=False):
+    """Prepares training and test datasets for Keras, with option
+    to only prepare 'identity' section of test data."""
+    test_index = list(x_test.index)
+    test = one_hot_results[test_index]
+    label_test = target_onehot[test_index]
+    if group:
+        return test, label_test
+    train = np.delete(one_hot_results, test_index, 0)
+    label_train = np.delete(target_onehot, test_index, 0)
+    return train, label_train, test, label_test
+
+
+def keras_model(train, label_train, epoch):
+    """Creates Keras model and results"""
+    model = models.Sequential()
+    model.add(Dropout(.2))
+    model.add(layers.Dense(1000, activation='relu', input_shape=(6000,)))
+    model.add(Dropout(.2))
+    model.add(layers.Dense(500, activation='relu'))
+    model.add(Dropout(.2))
+    model.add(layers.Dense(100, activation='relu'))
+    model.add(Dropout(.2))
+    model.add(layers.Dense(50, activation='relu'))
+    model.add(Dropout(.2))
+    model.add(layers.Dense(4, activation='softmax'))
+    model.compile(optimizer='SGD',
+                  loss='categorical_crossentropy',
+                  metrics=[f1_score_custom])
+    results = model.fit(train,
+                        label_train,
+                        epochs=epoch,
+                        batch_size=250,
+                        validation_split=0.2)
+    return model, results
+
+
+def model_evaluate(model, features, label, dataset='Training'):
+    """Evaluates entropy loss and F1-score when predicting target using model
+    against actual target data"""
+    results = model.evaluate(features, label)
+    print(f'{dataset} Entrpy Loss: {results[0]}')
+    print(f'{dataset} F1-Score: {results[1]}')
+
+
+def prediction_keras(model, test, y_test):
+    """Creates a target prediction array using Keras model, and converts
+    actual target data to array"""
+    y_pred_classes = model.predict_classes(test)
+    multi_label_test = np.array(y_test)
+    return y_pred_classes, multi_label_test
+
+
+def confusion_keras(y_pred_classes, multi_label_test, binary=False):
+    """Returns confusion matrix for Keras predictions"""
+    cm_val = confusion_matrix(multi_label_test, y_pred_classes)
+    cm_labels = confusion_matrix(multi_label_test, y_pred_classes)
+    if binary:
+        cm_labels = ['Good', 'Bad']
+    else:
+        cm_labels = ['Good_none', 'Good_group',
+                     'Bad_none', 'Bad_group']
+    plot_confusion_matrix(cm_val, cm_labels)
+
+
+def change(array):
+    """Changes array from multinary to binary"""
+    list_original = list(array)
+    list_new = []
+    for i in list_original:
+        if i in [0, 1]:
+            list_new.append(0)
+        elif i in [2, 3]:
+            list_new.append(1)
+    array_new = np.array(list_new)
+    return array_new
+
+
+def final_score(x_test, y_test, one_hot_results, target_onehot, model):
+    """Returns Keras model cost function score for only identity group test
+    data"""
+    x_group, y_group = keras_group(x_test, y_test)
+    test_group, label_group = keras_prep(one_hot_results, target_onehot,
+                                         x_group,
+                                         group=True)
+    y_pred_group, multi_label_group = prediction_keras(model, test_group,
+                                                       y_group)
+    cost_function(multi_label_group, y_pred_group, multinary=True)
